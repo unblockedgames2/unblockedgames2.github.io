8d2b191565ddc3e776bf9c2a28ed4549478ad0e9
unblockedgames2 <163449006+unblockedgames2@users.noreply.github.com>
2024-04-25 07:40:34 -0400

    fix gameover button

--- gamefiles/pixel_pioneers/game.js
+++ gamefiles/pixel_pioneers/game.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 // Game variables
-let log1 = "Add Favorites Section In Menu";
+let log1 = "Fix Gameover Button";
 let playerX = 400;
 let playerY = 350;
 let playerDirection = "down"; // Initial direction of the player
